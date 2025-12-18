5e2f84bf76a11aef51b8ac2fb70d059b1a98d499
Des Whelan <wheland6@tcd.ie>
2025-12-18 09:29:00 +0000

    initial commit

---
+++ config.example.py
@@ -0,0 +1,4 @@
+NUTRITION_AND_EXERCISE_API = {
+    "api_key": "ABC123",
+    "app_id": "ABC123"
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+import config
+
+API_KEY = config.NUTRITION_AND_EXERCISE_API["api_key"]
+APP_ID = config.NUTRITION_AND_EXERCISE_API["app_id"]
+
+# TODO: print the exercise stats for plain text input.
