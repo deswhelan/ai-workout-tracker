e4f3f04f4e337884f3e267ff8641f59eee61e66c
Des Whelan <wheland6@tcd.ie>
2025-12-18 09:53:08 +0000

    prompt user for exercise details and print applicable stats

--- config.example.py
+++ config.example.py
@@ -1,4 +1,12 @@
+# https://app.100daysofpython.dev/services/nutrition/docs
 NUTRITION_AND_EXERCISE_API = {
     "api_key": "ABC123",
     "app_id": "ABC123"
+}
+
+ATHLETE = {
+    "weight_kg": 75,
+    "height_cm": 175,
+    "age": 30,
+    "gender": female
 }
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import config
+import requests
 
 API_KEY = config.NUTRITION_AND_EXERCISE_API["api_key"]
 APP_ID = config.NUTRITION_AND_EXERCISE_API["app_id"]
 
 # TODO: print the exercise stats for plain text input.
+request_url = f"{config.NUTRITION_AND_EXERCISE_API["base_url"]}{config.NUTRITION_AND_EXERCISE_API["calories_burned_endpoint"]}"
+
+headers = {
+    "x-app-key": API_KEY,
+    "x-app-id": APP_ID
+}
+
+request_body = {
+    "query": str(input("Tell me which exercise you did:\n")),
+    "weight_kg": config.ATHLETE["weight_kg"],
+    "height_cm": config.ATHLETE["height_cm"],
+    "age": config.ATHLETE["age"],
+    "gender": config.ATHLETE["gender"]
+}
+
+response = requests.post(request_url, json=request_body, headers=headers)
+response.raise_for_status()
+
+print(response.text)
